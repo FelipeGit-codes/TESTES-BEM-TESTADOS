18f55b0a7e7ddad904e01e77e64765ea1bc363e5
FelipeGit-codes <andre.f.santos8@ba.estudante.senai.br>
2026-03-17 16:24:31 -0300

    Add files via upload

---
+++ Estudo_4.py
@@ -0,0 +1,17 @@
+import os
+import time
+
+os.system("cls")
+
+soma = 0
+QUANTIDADE_NOTAS = 5
+
+for i in range(QUANTIDADE_NOTAS):
+    nota = float(input("Digite uma nota: "))
+    soma += nota
+ 
+media = soma / QUANTIDADE_NOTAS
+
+print(f"Média: {media}")
+    
+
